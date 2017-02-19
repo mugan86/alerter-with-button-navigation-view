f10a12b0e195c3e83ac4b73980f4db057dbf37ca
anartzmugika <mugan86@gmail.com>
2017-02-19 08:07:02 +0100

    Add DashBoard Fragment

---
+++ app/src/main/java/com/amugika/alerter/DashBoardFragment.java
@@ -0,0 +1,24 @@
+package com.amugika.alerter;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**********************************************************
+ * Created by anartzmugika on 19/2/17.
+ */
+
+public class DashBoardFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+
+        //Inflate the layout for this fragment
+
+        return inflater.inflate(
+                R.layout.fragment_dashboard, container, false);
+    }
+}
