fe8222aa8be64cd12743ad7ea6c898d5927b3a8f
anartzmugika <mugan86@gmail.com>
2017-02-19 08:08:05 +0100

    Manage to open differents fragments

--- app/src/main/java/com/amugika/alerter/MainActivity.java
+++ app/src/main/java/com/amugika/alerter/MainActivity.java
@@ -3,13 +3,13 @@ package com.amugika.alerter;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +18,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    selectFragment(1);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    selectFragment(2);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    selectFragment(3);
                     return true;
             }
             return false;
@@ -37,9 +37,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        selectFragment(1);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    private void selectFragment(int item_select)
+    {
+
+        Fragment fragment = null;
+
+        if (item_select == 1) fragment = new MainFragment();
+        else if (item_select == 2) fragment = new DashBoardFragment();
+        else if (item_select == 3) fragment = new ContactFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction()
+                .replace(R.id.content, fragment).commit();
     }
 
 }
